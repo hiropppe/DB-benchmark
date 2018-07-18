b89cd857f54a7838b92e9bc7a1dd8c33d88141e0
hiropppe <sergei.0910@gmail.com>
2018-07-18 17:17:30 +0900

    Actually use private network for simply linking container and run cqlsh.

--- cassandra/csdr.sh
+++ cassandra/csdr.sh
@@ -1,3 +1,3 @@
 HOST_IP=`hostname -I | awk '{print $1}'`
-#docker run -d --name csdr --hostname csdr -e CASSANDRA_BROADCAST_ADDRESS=$HOST_IP -e CASSANDRA_SEEDS=$1 -p 9042:9042 -p 9160:9160 -p 7000:7000 cassandra:3.11.2
-docker run -d --name csdr$1 --hostname csdr$1 -e CASSANDRA_BROADCAST_ADDRESS=$HOST_IP -e CASSANDRA_SEEDS=$2 --net host cassandra:3.11.2
+docker run -d --name csdr --hostname csdr -e CASSANDRA_BROADCAST_ADDRESS=$HOST_IP -e CASSANDRA_SEEDS=$1 -p 9042:9042 -p 9160:9160 -p 7000:7000 cassandra:3.11.2
+#docker run -d --name csdr$1 --hostname csdr$1 -e CASSANDRA_BROADCAST_ADDRESS=$HOST_IP -e CASSANDRA_SEEDS=$2 --net host cassandra:3.11.2
